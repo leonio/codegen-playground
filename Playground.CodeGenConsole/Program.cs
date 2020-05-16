00e5ce98fccbcc7b21856372425c337b60aa57e5
leon.io <leonio@users.noreply.github.com>
2020-05-16 13:40:31 +0200

    add code

---
+++ Playground.CodeGenConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Playground.CodeGenConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playground.CodeGenerators.GeneratedClass.GeneratedMethod();
+            Console.WriteLine("Hello World from Console");
+        }
+    }
+}
---
+++ Playground.CodeGenerators/TestGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Playground.CodeGenerators
+{
+    [Generator]
+    public class TestGenerator : ISourceGenerator
+    {
+        public void Execute(SourceGeneratorContext context)
+        {
+            // begin creating the source we'll inject into the users compilation
+            var sourceBuilder = new StringBuilder(@"
+using System;
+namespace Playground.CodeGenerators
+{
+    public static class GeneratedClass
+    {
+        public static void GeneratedMethod() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+            Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
+");
+
+            // using the context, get a list of syntax trees in the users compilation
+            var syntaxTrees = context.Compilation.SyntaxTrees;
+
+            // add the filepath of each tree to the class we're building
+            foreach (var tree in syntaxTrees)
+            {
+                sourceBuilder.AppendLine($@"Console.WriteLine(@"" - {tree.FilePath}"");");
+            }
+
+            // finish creating the source to inject
+            sourceBuilder.Append(@"
+        }
+    }
+}");
+
+            // inject the created source into the users compilation
+            context.AddSource("testGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+
+        public void Initialize(InitializationContext context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
